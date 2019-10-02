dd4d88b2ea2f0d1de0c15bee19b151c4b28faac1
Danu <danuyaranal@gmail.com>
2019-10-02 16:09:45 +0530

    string datatypes

---
+++ app.js
@@ -0,0 +1,13 @@
+var str1="Danu";
+var str2='Ammu';
+//var str3=`Hi Danu and Ammu`;  //Template string (es6)
+
+var str3=`
+<h1>hello</h1>
+<p>lorem diouehbfkjjskbfjgrueioaqiaandmnalwiaoiwueihioan</p>
+<button>Submit</button>
+<input type="text" placeholder="Enter the text"/>`
+; //Template string (es6)
+document.write (str2 + str3);
+
+//console.log (str1 + str2 + str3);
